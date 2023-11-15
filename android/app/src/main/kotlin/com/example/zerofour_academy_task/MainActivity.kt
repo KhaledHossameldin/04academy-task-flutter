a54bb3e866f5a51e526ac5fe63542aef76a606f6
Khaled Hossameldin <33874711+KhaledHossameldin@users.noreply.github.com>
2023-11-15 15:11:12 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/zerofour_academy_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zerofour_academy_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'presentation/screens/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '04Academy Task',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
